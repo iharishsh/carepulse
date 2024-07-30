7122d6a2cf956146e496ff33a91aee58fed69bac
iharishsh <iharishsh@gmail.com>
2024-07-30 21:00:03 +0530

    setup appwrite

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -4,13 +4,13 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
-import { useRouter } from "next/router";
+import { useRouter } from 'next/navigation'
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -23,7 +23,7 @@ export enum FormFieldType {
 }
 
 const PatientForm = () => {
-    const router = useRouter();
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof UserFormValidation>>({
@@ -38,9 +38,9 @@ const PatientForm = () => {
   async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
     setIsLoading(true);
     try {
-        // const userData = { name, email, phone };
-        // const user = await createuser(userData);
-        // if(user) router.push(`/patients/${user.$id}/register`)
+        const userData = { name, email, phone };
+        const user = await createUser(userData);
+        if(user) router.push(`/patients/${user.$id}/register`)
     } catch (error) {
         console.log(error);
     }
