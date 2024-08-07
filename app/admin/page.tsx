6f0a8a3aea3337d04d1ccfbc984c3110861d5117
iharishsh <iharishsh@gmail.com>
2024-08-07 21:22:22 +0530

    Admin - getRecentAppointmentList

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,12 @@
 import StatCard from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Admin = () => {
+const Admin = async () => {
+    const appointments = await getRecentAppointmentList();
+
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
         <header className='admin-header'>
@@ -28,19 +31,19 @@ const Admin = () => {
             <section className='admin-stat'>
                 <StatCard 
                     type="appointments"
-                    count={5}
+                    count={appointments.scheduledCount}
                     label="Scheduled appointments"
                     icon="/assets/icons/appointments.svg"
                 />
                 <StatCard 
                     type="pending"
-                    count={10}
+                    count={appointments.pendingCount}
                     label="Pending appointments"
                     icon="/assets/icons/pending.svg"
                 />
                 <StatCard 
                     type="cancelled"
-                    count={2}
+                    count={appointments.cancelledCount}
                     label="Cancelled appointments"
                     icon="/assets/icons/cancelled.svg"
                 />
